f4ad73069ba3bd65f0718b3bf382f68c01ca2770
sengyungquek <yungquek@gmail.com>
2022-03-31 01:52:04 +0800

    Day-3 Update Headers

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,57 @@
 <template>
-  <div class="m-auto">
-    <style>
-      img {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-      }
-      u {
-        text-decoration: underline;
-      }
-    </style>
-    <img
-      src="https://previews.123rf.com/images/rawpixel/rawpixel1705/rawpixel170545869/78475053-learning-fun-childhood-imagination-education.jpg"
-      width="600"
-      height="700"
-      alt="Study Photo"
-    />
-    <h1 style="text-align: center" class="text-5xl">Welcome to <b>FUN LEARN</b></h1>
-    <p style="text-align: center" class="mt-5">
-      <i
-        >A Place to turn your <b>ideas</b> into
-        <a href="https://fishcate.com/jobs/salary/"
-          ><b style="color: blue; font-size: 25px"><u>REALITY</u></b></a
-        >...</i
-      >
-    </p>
-    <p style="text-align: center" class="mt-5">
-      <i>You can make this world a better place by...</i
-      ><a href="https://fishcate.com/"
-        ><b style="color: blue; font-size: 25px"><u>SHARING</u></b>
-      </a>
-    </p>
+  <div id="Main">
+  <div id="header" class="bg-[url('/hero-bg.jpg')] pt-5">
+    <div id="menu">
+      <div id="menu-left">
+        <img
+          src="ideaLogo.png"
+          class="w-[200px]"
+          alt=""
+          style="float: left; width: 100px; height: 100px ;py-99"
+        />
+      </div>
+    </div>
+    <div id="menu-right">
+      <center>
+        <ul class="grid grid-cols-4 text-lg font-oswald text-white uppercase mt-99">
+          <li>About Us</li>
+          <li>Courses</li>
+          <li>Coming Events</li>
+          <li>Contacts</li>
+        </ul>
+      </center>
+    </div>
+  </div>
+    <div class="m-auto">
+      <style>
+        u {
+          text-decoration: underline;
+        }
+      </style>
+      <center>
+        <img
+          src="https://previews.123rf.com/images/rawpixel/rawpixel1705/rawpixel170545869/78475053-learning-fun-childhood-imagination-education.jpg"
+          width="600"
+          height="700"
+          alt="Study Photo"
+        />
+      </center>
+      <h1 style="text-align: center" class="text-5xl">Welcome to <b>FUN LEARN</b></h1>
+      <p style="text-align: center" class="mt-5">
+        <i
+          >A Place to turn your <b>ideas</b> into
+          <a href="https://fishcate.com/jobs/salary/"
+            ><b style="color: blue; font-size: 25px"><u>REALITY</u></b></a
+          >...</i
+        >
+      </p>
+      <p style="text-align: center" class="mt-5">
+        <i>You can make this world a better place by...</i
+        ><a href="https://fishcate.com/"
+          ><b style="color: blue; font-size: 25px"><u>SHARING</u></b>
+        </a>
+      </p>
+    </div>
+  </div>
   </div>
 </template>
