1a1c4caf8347767e112c26521dc8df0cc522f6ec
sengyungquek <yungquek@gmail.com>
2022-03-31 23:08:50 +0800

    Day-3 Add Buttons and background color

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,70 @@
 <template>
   <div id="Main">
-  <div id="header" class="bg-[url('/hero-bg.jpg')] pt-5">
-    <div id="menu">
-      <div id="menu-left">
-        <img
-          src="ideaLogo.png"
-          class="w-[200px]"
-          alt=""
-          style="float: left; width: 100px; height: 100px ;py-99"
-        />
+    <div id="header" class="bg-[url('/hero-bg.jpg')] pt-5">
+      <div id="menu" class="grid-grid-cols-2">
+        <div id="menu-left">
+          <img
+            src="ideaLogo.png"
+            class="mt-1"
+            alt=""
+            style="float: left; width: 100px; height: 100px"
+          />
+        </div>
+        <div id="menu-right">
+          <ul
+            class="grid grid-cols-4 text-lg font-oswald text-white uppercase mt-99 pb-5 pt-5"
+          >
+            <li>About Us</li>
+            <li>Knowledge Hub</li>
+            <li>Coming Events</li>
+            <li>Contacts</li>
+          </ul>
+        </div>
       </div>
     </div>
-    <div id="menu-right">
-      <center>
-        <ul class="grid grid-cols-4 text-lg font-oswald text-white uppercase mt-99">
-          <li>About Us</li>
-          <li>Courses</li>
-          <li>Coming Events</li>
-          <li>Contacts</li>
-        </ul>
-      </center>
+    <div id="header" class="bg-[url('/BlueImage.jpg')] pt-5">
+      <div class="m-auto">
+        <style>
+          u {
+            text-decoration: underline;
+          }
+        </style>
+        <center>
+          <img
+            src="ideaLogo.png"
+            width="300"
+            height="300"
+            alt="Study Photo"
+            class="center"
+          />
+        </center>
+        <h1 style="text-align: center" class="text-5xl font-roboto pt-5">
+          Welcome to <b>FUN LEARN</b>
+        </h1>
+        <p style="text-align: center" class="mt-5">
+          <i
+            >A Place to turn your <b>ideas</b> into
+            <a href="https://fishcate.com/jobs/salary/"
+              ><b style="color: blue; font-size: 25px"><u>REALITY</u></b></a
+            >...</i
+          >
+        </p>
+        <p style="text-align: center" class="mt-5">
+          <i>You can make this world a better place by...</i
+          ><a href="https://fishcate.com/"
+            ><b style="color: blue; font-size: 25px"><u>SHARING</u></b>
+          </a>
+        </p>
+      </div>
     </div>
-  </div>
-    <div class="m-auto">
-      <style>
-        u {
-          text-decoration: underline;
-        }
-      </style>
-      <center>
-        <img
-          src="https://previews.123rf.com/images/rawpixel/rawpixel1705/rawpixel170545869/78475053-learning-fun-childhood-imagination-education.jpg"
-          width="600"
-          height="700"
-          alt="Study Photo"
-        />
-      </center>
-      <h1 style="text-align: center" class="text-5xl">Welcome to <b>FUN LEARN</b></h1>
-      <p style="text-align: center" class="mt-5">
-        <i
-          >A Place to turn your <b>ideas</b> into
-          <a href="https://fishcate.com/jobs/salary/"
-            ><b style="color: blue; font-size: 25px"><u>REALITY</u></b></a
-          >...</i
-        >
-      </p>
-      <p style="text-align: center" class="mt-5">
-        <i>You can make this world a better place by...</i
-        ><a href="https://fishcate.com/"
-          ><b style="color: blue; font-size: 25px"><u>SHARING</u></b>
-        </a>
-      </p>
+    <div id="section-1" class="text-center bg-[url('/ShareImage.jpg')] pb-60 bg-cover">
+      <h1 class="font-robot font light text-2xl text-center px-20 py-5">
+        Knowledge is powerful. It's much more powerful if you are willing to share this
+        with others. Please feel free to share your ideas with others
+      </h1>
+      <button class="font-oswald uppercase bg-blue-500 text-white text-center py-5 px-8">
+        Any Ideas to Share?
+      </button>
     </div>
-  </div>
   </div>
 </template>
