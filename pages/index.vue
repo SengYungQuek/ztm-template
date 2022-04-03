c3b5aa4addff7d51c4c22ec57c22ac781dbbfde0
sengyungquek <yungquek@gmail.com>
2022-04-03 23:39:25 +0800

    Day-5 Changes to Tailwinds

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,7 @@
     <div id="header" class="bg-[url('/hero-bg.jpg')] pt-5">
       <div id="menu" class="grid-grid-cols-2">
         <div id="menu-left">
-          <img
-            src="ideaLogo.png"
-            class="mt-1"
-            alt=""
-            style="float: left; width: 100px; height: 100px"
-          />
+          <img src="ideaLogo.png" class="mt-1 float-left w-[100px] y-100[px]" alt="" />
         </div>
         <div id="menu-right">
           <ul
@@ -24,18 +19,11 @@
     </div>
     <div id="header" class="bg-[url('/BlueImage.jpg')] pt-5">
       <div class="m-auto">
-        <style>
-          u {
-            text-decoration: underline;
-          }
-        </style>
         <center>
           <img
             src="ideaLogo.png"
-            width="300"
-            height="300"
             alt="Study Photo"
-            class="center animate-bounce"
+            class="center animate-bounce h-[400px] w-[400px]"
           />
         </center>
         <h1 style="text-align: center" class="text-5xl font-roboto pt-5">
@@ -45,97 +33,63 @@
           <i
             >A Place to turn your <b>ideas</b> into
             <a href="https://fishcate.com/jobs/salary/"
-              ><b style="color: blue; font-size: 25px"><u>REALITY</u></b></a
+              ><b class="underline text-[25px] text-blue-600">REALITY</b></a
             >...</i
           >
         </p>
         <p style="text-align: center" class="mt-5">
           <i>You can make this world a better place by...</i
           ><a href="https://fishcate.com/"
-            ><b style="color: blue; font-size: 25px"><u>SHARING</u></b>
+            ><b class="underline text-[25px] text-blue-600">SHARING</b>
           </a>
+          <br />
+          <br />
         </p>
       </div>
     </div>
     <div
       id="section-1"
-      class="text-center bg-[url('/ShareImage.jpg')] pb-20 bg-cover bg-center"
+      class="text-center bg-[url('/ShareImage.jpg')] pb-20 bg-cover bg-center Static"
     >
-      <h1>.</h1>
-      <h1>.</h1>
-      <button class="font-oswald uppercase bg-blue-400 text-white text-center py-5 px-10">
+      <br />
+      <br />
+      <br />
+      <br />
+      <button
+        class="font-oswald uppercase bg-blue-400 text-white text-center rounded text-3xl bottom-6 border-8 border-blue-400"
+      >
         Any Ideas to Share?
       </button>
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
-    <h1 style="" class="text-2xl font-roboto pt-5">Resources Available:</h1>
+    <h1 style="" class="text-2xl font-roboto pt-5 underline">
+      <b>Resources Available:</b>
+    </h1>
     <div id="courses" class="grid grid-cols-4 text-lg font-oswald">
       <div id="Courses-col1">
-        <img
-          src="Accounting.jfif"
-          alt="Accounting"
-          style="width: 400px; height: 200px"
-          class="mt-1"
-        />
-        <img
-          src="Investment.jfif"
-          alt="Inestment"
-          style="width: 400px; height: 200px"
-          class="mt-1"
-        />
-        <img
-          src="Excel.png"
-          alt="Excel"
-          style="width: 400px; height: 200px"
-          class="mt-1"
-        />
+        <img src="Accounting.jfif" alt="Accounting" class="mt-1 h-[200px] w-[400px]" />
+        <img src="Investment.jfif" alt="Inestment" class="mt-1 h-[200px] w-[400px]" />
+        <img src="Excel.png" alt="Excel" class="mt-1 h-[200px] w-[400px]" />
       </div>
       <div id="Courses-col2">
-        <img
-          src="Branding.jfif"
-          alt="Branding"
-          style="width: 400px; height: 200px"
-          class="mt-1"
-        />
-        <img
-          src="Maths.jfif"
-          alt="Maths"
-          style="width: 400px; height: 200px"
-          class="mt-1"
-        />
-        <img
-          src="PowerBI.png"
-          alt="PowerBI"
-          style="width: 400px; height: 200px"
-          class="mt-1"
-        />
+        <img src="Branding.jfif" alt="Branding" class="mt-1 h-[200px] w-[400px]" />
+        <img src="Maths.jfif" alt="Maths" class="mt-1 h-[200px] w-[400px]" />
+        <img src="PowerBI.png" alt="PowerBI" class="mt-1 h-[200px] w-[400px]" />
       </div>
       <div id="Courses-col3">
-        <img
-          src="Coding.jfif"
-          alt="Coding"
-          style="width: 400px; height: 200px"
-          class="mt-1"
-        />
+        <img src="Coding.jfif" alt="Coding" class="mt-1 h-[200px] w-[400px]" />
         <img
           src="OnlineMarketing.jfif"
           alt="OnlineMarketing"
-          style="width: 400px; height: 200px"
-          class="mt-1"
+          class="mt-1 h-[200px] w-[400px]"
         />
       </div>
       <div id="Courses-col4">
-        <img
-          src="Cooking.jfif"
-          alt="Cooking"
-          style="width: 400px; height: 200px"
-          class="mt-1"
-        />
-        <img
-          src="Economics.jfif"
-          alt="Economics"
-          style="width: 400px; height: 200px"
-          class="mt-1"
-        />
+        <img src="Cooking.jfif" alt="Cooking" class="mt-1 h-[200px] w-[400px]" />
+        <img src="Economics.jfif" alt="Economics" class="mt-1 h-[200px] w-[400px]" />
       </div>
     </div>
   </div>
